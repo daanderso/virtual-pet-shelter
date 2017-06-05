eb350122a7ee045b784277b45ccc4e7a6c13f5df
Derek <daanderso@gmail.com>
2017-06-04 23:13:12 -0400

    VirtualPetShelterApp

--- src/VirtualPetShelterApp.java
+++ src/VirtualPetShelterApp.java
@@ -1,6 +1,3 @@
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class VirtualPetShelterApp {
@@ -9,16 +6,14 @@ public class VirtualPetShelterApp {
 		Scanner input = new Scanner(System.in);
 
 		// Creating VirtualPet objects
-		VirtualPet scout = new VirtualPet("Scout", "lab", 10, 20, 30);
-		VirtualPet rubble = new VirtualPet("Rubble", "yorkie", 20, 30, 40);
-		// VirtualPet chase = new VirtualPet("Chase", "bull Dog", 50, 40, 30);
-		// VirtualPet zuma = new VirtualPet("Zuma", "lab", 15, 30, 45);
+		VirtualPet scout = new VirtualPet("scout", "lab", 50, 40, 30);
+		VirtualPet rubble = new VirtualPet("rubble", "yorkie", 45, 50, 40);
 
-		// creating the VirtualPetShelter object shelter
+
+		// creating the VirtualPetShelter object shelter 
 		VirtualPetShelter shelter = new VirtualPetShelter();
 
-		// adding to pets map
-		// shelterPets.put("Rubble", rubble);
+		// adding to pets to add to populate map
 		shelter.addPet(scout);
 		shelter.addPet(rubble);
 
@@ -28,24 +23,16 @@ public class VirtualPetShelterApp {
 		// Display the status here
 		writeLine("Name\tHunger\tthirst\tBoredom");
 		writeLine("-----\t------\t------\t--------");
-
-		// Display
 		for (VirtualPet current : shelter.pets()) {
 			System.out.println(
 					current.name + " " + "\t  " + current.hunger + "\t  " + current.thirst + "\t  " + current.boredom);
 		}
 
-		// testing getpet values method for status
-		// System.out.println(shelter.getPetStatus());
-
-		// Retrieving a status of what's in the map
-		// System.out.println(shelter.getMap()); // test
-
 		// BEGIN WHILE GAME LOOP
 		boolean startGame = false;
 		while (!startGame) {
 			// Interaction;
-			writeLine("\nWhat do you want to do ?\n");
+			writeLine("\nWhat do you want to do ? Enter a number 1-6.\n");
 			writeLine("1. Feed the pets");
 			writeLine("2. Water the pets");
 			writeLine("3. Play with a pet");
@@ -57,59 +44,60 @@ public class VirtualPetShelterApp {
 
 			switch (enterAction) {
 			case "1":
-				writeLine("Testing 123");
 				// Feed All Pets
 				shelter.feedAllPets();
-				System.out.println(shelter.getMap()); // testing
 				break;
 			case "2":
-				writeLine("Testing 123");
 				// Give All Pets water
 				shelter.waterAllPets();
-				// System.out.println(shelter.getMap()); // testing
 				break;
 			case "3":
 				// Play with A Pet
 				writeLine("Who would you like to play with? Type a name.\n");
 
 				// for loop is calling a method pets that goes through the
-				// values of the current map
+				// values of the current map and displays name and description
 				for (VirtualPet current : shelter.pets()) {
-					writeLine("[" + current.name + "] " + current.petBreed);
+					writeLine("[" + current.name + "] " + current.description);
 				}
 				String petNameInput = input.next();
 
-				VirtualPet petToPlay = shelter.getPet(petNameInput);
+				//getting a pet from the map to pass to play method
+				VirtualPet petToPlay = shelter.getPet(petNameInput.toLowerCase());
 				shelter.playWithOnePet(petToPlay);
 
 				writeLine("You played with " + petNameInput + ". ");
 				break;
-
 			case "4":
-				writeLine("Testing 123");
 				// Adopt a pet
 				writeLine("Which charming pet would you like to adopt?");
+				
+				//Display available pets to adopt
+				for (VirtualPet current : shelter.pets()) {
+					writeLine("[" + current.name + "] " + current.description);
+				}
+				
 				String adoptNameInput = input.next();
-
-				shelter.adoptPet(adoptNameInput);
-				System.out.println(shelter.getMap());
+				shelter.adoptPet(adoptNameInput.toLowerCase());
 
 				break;
 			case "5":
-
 				// Admit a pet
 				writeLine("What is the admitted pets name?");
 				String admittedPetName = input.next();
 				writeLine("What breed is your pet?");
-				String admittedPetBreed = input.next();
+				String admittedPetDescription = input.next(); 
+			
+		
+				//changing input to lower to avoid any issues with key
+				String admittedPetNameLower = admittedPetName.toLowerCase();
 
-				// create a new constructor object for admitted pet
-				VirtualPet admittedPet = new VirtualPet(admittedPetName, admittedPetBreed);
-
-				// adding to shelter
+				// create a new constructor VirtualPet object  admittedPet to pass to addPet() method
+				VirtualPet admittedPet = new VirtualPet(admittedPetNameLower, admittedPetDescription);
+				
+				// adding a pet to shelter map
 				shelter.addPet(admittedPet);
-				System.out.println(shelter.getMap());
-
+				
 				break;
 			case "6":
 				writeLine(" You have exited the app");
@@ -117,7 +105,7 @@ public class VirtualPetShelterApp {
 				break;
 			}// end switch case
 
-			// Displaying
+			// Displaying Pet Status
 			writeLine("Name\tHunger\tthirst\tBoredom");
 			writeLine("-----\t------\t------\t--------");
 
@@ -128,14 +116,15 @@ public class VirtualPetShelterApp {
 
 			// Tick method for all pets
 			shelter.tickAllPets();
-			System.out.println(shelter.getMap()); // testing
 
 		} // END WHILE
+		
+		input.close();
 
-	}// end main method
+	}// End Main Method
 
 	public static void writeLine(String message) {
 		System.out.println(message);
 	}
 
-}// end class
+}// End Class
