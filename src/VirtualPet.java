22d56a24455504619a5535683a619bcae70509bc
Derek <daanderso@gmail.com>
2017-06-04 23:08:09 -0400

    VirtualPet class

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -2,34 +2,25 @@ import java.util.Random;
 
 public class VirtualPet {
 	String name;
-	String petBreed;
 	String description;
 	int hunger;
 	int thirst;
 	int boredom;
-	int tired;
-	
-	boolean isHungry;
-	boolean isThirsty;
-	boolean isBored;
-	boolean isTired;
-	boolean death; // boolean to start while loop if the pet is alive
-
-	// creating constructors accepting name and breed so they do not set the
-	// default value
-	public VirtualPet(String newName, String newBreed) {
+ 
+	// creating constructors accepting name and breed to set the default value
+	public VirtualPet(String newName, String newDescription) {
 		name = newName;
-		petBreed = newBreed;
+		description = newDescription;
 		hunger = 45;
 		thirst = 25;
 		boredom = 40;
 	}
 
-	// creating constructor that passes in name, newBreed, hunger, thirst,
+	// creating constructor that passes in name, description, hunger, thirst,
 	// boredom
-	public VirtualPet(String newName, String newBreed, int newHunger, int newThirst, int newBoredom) {
+	public VirtualPet(String newName, String newDescription, int newHunger, int newThirst, int newBoredom) {
 		name = newName;
-		petBreed = newBreed;
+		description = newDescription;
 		hunger = newHunger;
 		thirst = newThirst;
 		boredom = newBoredom;
@@ -38,8 +29,6 @@ public class VirtualPet {
 
 	// creating random object r for random number generator in tick method
 	Random r = new Random();
-
-	//get pet stats
 	
 	
 	// Methods for a hungry pet
@@ -48,59 +37,17 @@ public class VirtualPet {
 		thirst = thirst + 5;
 	}
 
-	boolean isHungry() {
-
-		return hunger >= 50;
-
-	}
-
-	int getHunger() {
-		return hunger;
-	}
-
 	// Methods for a thirsty pet
-
 	void water() {
 		thirst = thirst - 10;
-		tired = tired + 7;
 	}
 
-	boolean isThirsty() {
-		return thirst >= 50;
-	}
-
-	int getThirst() {
-		return thirst;
-	}
 
 	// Methods for a bored pet
 	void play() {
 		boredom = boredom - 10;
 		hunger = hunger + 5;
 		thirst = thirst + 4;
-		tired = tired + 5;
-	}
-
-	boolean isBored() {
-		return boredom >= 50;
-	}
-
-	int getPlayfulness() {
-		return boredom;
-	}
-
-	// Methods for a tired pet
-	void nap() {
-		tired = tired - 20;
-		hunger = hunger + 10;
-	}
-
-	boolean isTired() {
-		return tired >= 50;
-	}
-
-	int getTiredness() {
-		return tired;
 	}
 
 	// Tick method
@@ -109,13 +56,12 @@ public class VirtualPet {
 		hunger = hunger + r.nextInt(10);
 		thirst = thirst + r.nextInt(10);
 		boredom = boredom + r.nextInt(10);
-		tired = tired + r.nextInt(15);
 
 	}
 	
 	@Override //used to turn object into String  ( toString)
 	public String toString() {
-		return name +" "+ petBreed +" " +  hunger + " " + thirst +" "+ boredom ;
+		return name +" "+ description +" " +  hunger + " " + thirst +" "+ boredom ;
 	}
 
 }// end class bracket
